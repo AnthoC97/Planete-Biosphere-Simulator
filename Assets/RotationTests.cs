a6469ae2d0d427fbb9d8d667f4135bc5de32ab37
Florian CHAMPAUD <fchampaud@kyriopolis.fr>
2020-06-25 17:23:23 +0200

    Add rotation tests scene and script

---
+++ Assets/RotationTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTests : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject cylinder = GameObject.Find("Cylinder");
+        Vector3 point = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            cylinder.transform.Rotate(new Vector3(0, -.5f, 0));
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            cylinder.transform.Rotate(new Vector3(0, .5f, 0));
+        }
+
+        cylinder.transform.RotateAround(point, cylinder.transform.right,
+                30 * Time.deltaTime);
+        //cylinder.transform.RotateAround(point, cylinder.transform.right, 0);
+    }
+}

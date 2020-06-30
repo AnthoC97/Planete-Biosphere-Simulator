011248b3af9eed51a812623644cac10bc1a43039
Anthony CONTREVILLIERS <anthony_contrevilliers@yahoo.fr>
2020-06-30 16:48:15 +0200

    Beginning of GA on trees

---
+++ Assets/Scripts/Vegetation/Tree.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    float height;
+    float radius;
+    float depth;
+    float glucid; //between 0 and  1
+    float water; //between 0 and  1
+    float minerals; //between 0 and  1
+
+    void Photosynthesis(int lux)
+    {
+
+    }
+
+    void Evapotranspiration(float temperature, float humidity)
+    {
+
+    }
+}
